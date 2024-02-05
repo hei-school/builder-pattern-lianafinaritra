4c801c127e2e7de4fe533d373514accf1cd8abb1
lianafinaritra <hei.lianafinaritra@gmail.com>
2024-02-05 17:48:39 +0300

    feat: implement engine class

---
+++ src/main/java/com/lianafinaritra/buildpattern/model/Engine.java
@@ -0,0 +1,13 @@
+package com.lianafinaritra.buildpattern.model;
+
+public class Engine {
+    private final String name;
+
+    public Engine(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
