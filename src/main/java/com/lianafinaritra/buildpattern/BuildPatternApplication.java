f45c4d6c3d19a1df41f142a6af5c30a25d1e4f3d
lianafinaritraratovo <hei.finaritra@gmail.com>
2024-02-05 17:30:57 +0300

    feat: initialize project

---
+++ src/main/java/com/lianafinaritra/buildpattern/BuildPatternApplication.java
@@ -0,0 +1,13 @@
+package com.lianafinaritra.buildpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildPatternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lianafinaritra/buildpattern/BuildPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lianafinaritra.buildpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
